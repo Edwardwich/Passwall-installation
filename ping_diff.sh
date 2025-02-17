6ed42a02eb316b5a29671a43642a889dbb1855c5
ali ghafourian <35195176+Edwardwich@users.noreply.github.com>
2025-02-18 01:09:57 +0330

    Update ping_diff.sh

--- ping_diff.sh
+++ ping_diff.sh
@@ -3,8 +3,10 @@
 # Function to perform the ping tests and show the results
 ping_tests() {
     # IP addresses of Router and ADSL Modem
-    ROUTER_IP="192.168.2.1"
-    ADSL_IP="192.168.1.1"
+    # ROUTER_IP="192.168.2.1"
+    # ADSL_IP="192.168.1.1"
+    TWITCH_IP="twitch.tv"
+    YOUTUBE_IP="youtube.com"
     GOOGLE_IP="8.8.8.8"
     GAME_IP="4.2.2.4"
 
@@ -28,11 +30,15 @@ ping_tests() {
     # Get the ping to Google
     GOOGLE_PING=$(ping_result $GOOGLE_IP)
 
+    # Get the ping to the TWITCH_IP Modem  YOUTUBE_IP
+    TWITCH_PING=$(ping_result $TWITCH_IP)
+    YOUTUBE_PING=$(ping_result $YOUTUBE_IP)
+
     # Get the ping to the ADSL Modem
-    ADSL_PING=$(ping_result $ADSL_IP)
+    # ADSL_PING=$(ping_result $ADSL_IP)
 
     # Get the ping to the Router
-    ROUTER_PING=$(ping_result $ROUTER_IP)
+    # ROUTER_PING=$(ping_result $ROUTER_IP)
 
     # Calculate the modem delay excluding router delay if both are available
     if [[ "$ADSL_PING" != "Ping failed" && "$ROUTER_PING" != "Ping failed" ]]; then
@@ -44,9 +50,12 @@ ping_tests() {
     # Create a message for the dialog box
     RESULTS="Google Ping: $GOOGLE_PING ms\n"
     RESULTS+="4.2.2.4 Ping: $GAME_PING ms\n"
-    RESULTS+="ADSL Modem Ping: $ADSL_PING ms\n"
-    RESULTS+="Router Ping: $ROUTER_PING ms\n"
-    RESULTS+="(Modem-Router): $DIFFERENCE ms"
+    RESULTS+="twitch Ping: $TWITCH_PING ms\n"
+    RESULTS+="youtube Ping: $YOUTUBE_PING ms\n"
+
+    # RESULTS+="ADSL Modem Ping: $ADSL_PING ms\n" TWITCH_PING
+    # RESULTS+="Router Ping: $ROUTER_PING ms\n"
+    # RESULTS+="(Modem-Router): $DIFFERENCE ms"
 
     # Clear previous results and display new ones using dialog without OK button
     dialog --title "Ping Results" --infobox "$RESULTS" 10 50

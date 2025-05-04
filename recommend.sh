c4cc7a3fc2d637b873c22b442287b87a3d084d14
ali ghafourian <35195176+Edwardwich@users.noreply.github.com>
2025-05-04 12:19:26 +0330

    Add files via upload

---
+++ recommend.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+# First Reform
+opkg update
+opkg install kmod-nft-socket
+opkg install kmod-nft-tproxy
+opkg remove dnsmasq
+opkg install dnsmasq-full
+opkg install
+
+
+# Function to install a theme
+install_theme() {
+  local REPO_NAME=$1
+  local THEME_NAME=$2
+
+  echo "Processing $THEME_NAME..."
+    #https://github.com/jerrykuku/luci-theme-argon/releases
+  # GitHub repository URL and package name
+  LATEST_RELEASE_URL="https://api.github.com/repos/jerrykuku/luci-theme-argon/releases/latest"
+  IPK_URL=$(curl -s $LATEST_RELEASE_URL | grep "browser_download_url.*ipk" | cut -d '"' -f 4)
+
+  # Check if the download link is found
+  if [ -z "$IPK_URL" ]; then
+    echo "Download link for the .ipk file of luci-theme-argon not found."
+    return 1
+  fi
+
+  # Download the .ipk package
+  echo "Downloading the latest version of $THEME_NAME..."
+  wget -q $IPK_URL -O /tmp/$THEME_NAME.ipk
+
+  # Install the .ipk package
+  echo "Installing luci-theme-argon..."
+  opkg install /tmp/luci-theme-argon.ipk
+
+  # Clean up the downloaded file
+  rm /tmp/luci-theme-argon.ipk
+
+  echo "luci-theme-argon installed successfully."
+}
+
+# Install luci-theme-peditx
+install_theme "luci-theme-argon"
+
+
+
+
+# Get all supported architectures from opkg
+arch_list=$(opkg print-architecture | awk '{print $2}')
+
+# Find the first matching architecture in the available package list
+pkg_arch=""
+for arch in $arch_list; do
+    case "$arch" in
+        aarch64_cortex-a53|aarch64_cortex-a72|aarch64_generic|\
+        arm_cortex-a15_neon-vfpv4|arm_cortex-a5_vfpv4|arm_cortex-a7|\
+        arm_cortex-a7_neon-vfpv4|arm_cortex-a8_vfpv3|arm_cortex-a9|\
+        arm_cortex-a9_neon|arm_cortex-a9_vfpv3-d16|mipsel_24kc|\
+        mipsel_74kc|mipsel_mips32|mips_24kc|mips_4kec|mips_mips32|x86_64)
+            pkg_arch="$arch"
+            break
+            ;;
+    esac
+done
+
+if [ -z "$pkg_arch" ]; then
+    echo "Unsupported CPU architecture detected!"
+    exit 1
+fi
+
